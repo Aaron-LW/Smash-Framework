bbb3034bd096fd38278c27c0dd2465566ec4d455
Aaron_LW <aaronl.waechter@gmail.com>
2026-02-20 23:13:19 +0100

    Add MathHelper

---
+++ Smash/Framework/MathHelper.cs
@@ -0,0 +1,14 @@
+using System.Numerics;
+
+public static class MathHelper
+{
+    public static float Lerp(float startValue, float endValue, float time)
+    {
+        return startValue + (endValue - startValue) * time;
+    }
+
+    public static Vector2 LerpVector2(Vector2 startValue, Vector2 endValue, float time)
+    {
+        return startValue + (endValue - startValue) * time;
+    }
+}
